87a7962436944f1fe154001afc2ec9ea32dcb5a8
https://github.com/Designelegant <designelegant31@gmail.com>
2026-07-15 11:16:49 +0530

    Stop tracking Python files

--- get_all_decrypted_urls.py
+++
@@ -1,7 +0,0 @@
-with open("decrypted_content.txt", "r", encoding="utf-8") as f:
-    text = f.read()
-
-import re
-urls = re.findall(r"https?://[^\s]+", text)
-for u in sorted(set(urls)):
-    print("Found URL in decrypted text:", u)
